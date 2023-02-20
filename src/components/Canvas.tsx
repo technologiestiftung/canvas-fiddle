cf2a714e4cc0e88524e376b0025a1a22877deda1
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-02-20 08:43:46 +0100

    feat: add baseline square element to canvas

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,26 @@
+import { FC, useEffect, useRef } from "react";
+
+const SQUARE_WIDTH = 150;
+
+export const Canvas: FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef || !canvasRef.current) return;
+
+    const ctx = canvasRef.current.getContext("2d");
+
+    const xPos = canvasRef.current.width / 2 - SQUARE_WIDTH / 2;
+    const yPos = canvasRef.current.height / 2 - SQUARE_WIDTH / 2;
+
+    ctx?.fillRect(xPos, yPos, SQUARE_WIDTH, SQUARE_WIDTH);
+  }, [canvasRef]);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={window.innerWidth}
+      height={window.innerHeight}
+    ></canvas>
+  );
+};
